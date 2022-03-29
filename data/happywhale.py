5bdcd95e98daec0e905bc6d4126faf8669ce4c27
jianzhnie <jianzhnie@126.com>
2022-03-29 20:57:21 +0800

    update pre-commit

--- data/happywhale.py
+++ data/happywhale.py
@@ -12,12 +12,15 @@ from torch.utils.data import Dataset
 
 
 class HappyWhaleDataset(Dataset):
-    def __init__(self, df, transform=None):
+    def __init__(self, df, train=True, transform=None):
         self.df = df
         self.transform = transform
         self.image_names = self.df['image'].values
         self.image_paths = self.df['image_path'].values
-        self.targets = self.df['individual_id'].values
+        if train:
+            self.targets = self.df['individual_id'].values
+        else:
+            self.targets = None
 
     def __getitem__(self, index):
         # 图片名字
@@ -30,7 +33,11 @@ class HappyWhaleDataset(Dataset):
         if self.transform:
             image = self.transform(image)
 
-        target = self.targets[index]
+        if self.targets:
+            target = self.targets[index]
+        else:
+            target = None
+
         target = torch.tensor(target, dtype=torch.long)
 
         return {'image_name': image_name, 'image': image, 'target': target}
