3d29c24ce73509e1e37cf3fc0c59c64eb4f7eb45
jianzheng.nie <jianzhnie@126.com>
2022-03-30 15:02:57 +0800

    update data

--- data/happywhale.py
+++ data/happywhale.py
@@ -1,28 +1,31 @@
 '''
 Author: jianzhnie
 Date: 2022-03-29 11:11:54
-LastEditTime: 2022-03-29 11:43:19
+LastEditTime: 2022-03-30 15:02:02
 LastEditors: jianzhnie
 Description:
 
 '''
+
+import pandas as pd
 import torch
 from PIL import Image
 from torch.utils.data import Dataset
+from typing import Optional
+from typing import Callable
+from typing import Dict
 
 
 class HappyWhaleDataset(Dataset):
-    def __init__(self, df, train=True, transform=None):
+    def __init__(self, df: pd.DataFrame, transform: Optional[Callable] = None):
         self.df = df
         self.transform = transform
-        self.image_names = self.df['image'].values
-        self.image_paths = self.df['image_path'].values
-        if train:
-            self.targets = self.df['individual_id'].values
-        else:
-            self.targets = None
 
-    def __getitem__(self, index):
+        self.image_names = self.df["image"].values
+        self.image_paths = self.df["image_path"].values
+        self.targets = self.df["individual_id"].values
+
+    def __getitem__(self, index: int) -> Dict[str, torch.Tensor]:
         # 图片名字
         image_name = self.image_names[index]
         # 图片路径
@@ -33,14 +36,10 @@ class HappyWhaleDataset(Dataset):
         if self.transform:
             image = self.transform(image)
 
-        if self.targets:
-            target = self.targets[index]
-        else:
-            target = None
-
+        target = self.targets[index]
         target = torch.tensor(target, dtype=torch.long)
 
-        return {'image_name': image_name, 'image': image, 'target': target}
+        return {"image_name": image_name, "image": image, "target": target}
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.df)
